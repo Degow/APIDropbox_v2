29b30f0145190a3fbee9e6defd469f5a39fa160e
degow <diegoamorinsr@gmail.com>
2017-11-27 18:28:25 -0500

    Subir y descargar archivos

---
+++ download_files_apidropbox.py
@@ -0,0 +1,16 @@
+"""
+EJEMPLOS:
+	TOKEN = Token de tu aplicación
+	ARCHIVO_LOCAL = Poner la ruta del archivo 
+					en el que se va almacenar.
+	RUTA_REMOTA = LA ruta donde se encuentra 
+					tu archivo en Dropbox.
+"""
+
+import dropbox
+
+dbx = dropbox.Dropbox("TOKEN")
+
+with open("<ARCHIVO_LOCAL>", "wb") as f:
+    md, res = dbx.files_download(path="/<RUTA_REMOTA>")
+    f.write(res.content)
---
+++ upload_files_apidropbox.py
@@ -0,0 +1,14 @@
+"""
+EJEMPLOS:
+	TOKEN = Token de tu aplicación
+	RUTA_ARCHIVO_LOCAL = Ruta donde se encuentra
+					a subir. 
+	RUTA_REMOTA = Ruta Dropbox para guardar archivo
+"""
+
+import dropbox
+
+dbx = dropbox.Dropbox("TOKEN")
+
+with open("<RUTA_ARCHIVO_LOCAL>", "rb") as f:
+	dbx.files_upload(f.read(), "/<RUTA_REMOTA>", mute=True)
