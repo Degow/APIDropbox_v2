c88960631ee31e53b451493909e3abc09ddf10a4
degow <diegoamorinsr@gmail.com>
2017-11-27 20:32:54 -0500

    Obtener metadatos de un archivo en Dropbox

---
+++ file_metadata_apidropbox.py
@@ -0,0 +1,12 @@
+import dropbox
+
+dbx = dropbox.Dropbox('<TOKEN>')
+md = dbx.files_get_metadata('/<RUTA_REMOTA>')
+
+nombre = md.name
+fecha = md.client_modified
+ubicacion = md.path_display
+
+print("Nombre del Archivo:\t", nombre)
+print("Fecha de registro:\t", fecha)
+print("Ubicacion del archivo:\t", ubicacion)
